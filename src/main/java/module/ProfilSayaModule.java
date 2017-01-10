99415de1a7a1ddf38670ae814b6119affb4dfd83
Buddy Arifin <buddy.arifin@olx.co.id>
2017-01-10 07:13:37 +0700

    Add Color Appender on Console Log

--- src/main/java/module/ProfilSayaModule.java
+++ src/main/java/module/ProfilSayaModule.java
@@ -603,6 +603,7 @@ public class ProfilSayaModule extends BasePage{
         for (int i = 0; i < list.size() ; i++) {
 
             if (list.get(i).getText().equalsIgnoreCase("Dropbox")) {
+                Log.debug("Found Target External Gallery APP : "+list.get(i).getText());
                 list.get(i).click();
                 break;
             }
--- src/main/java/utils/Log.java
+++ src/main/java/utils/Log.java
@@ -13,9 +13,11 @@ public class Log {
     private static PatternLayout layout = new PatternLayout("%d{dd MMM yyyy HH:mm:ss} %5p %c{1} - %m%n");
     private static ConsoleAppender consoleAppender;
     private static StepsScreenshotsRule screenshotsRule;
+    private static ANSIConsoleAppender ansiConsoleAppender;
 
     static {
         screenshotsRule = new StepsScreenshotsRule(layout, "System.out");
+        ansiConsoleAppender = new ANSIConsoleAppender(layout, "System.out");
     }
 
     /**
@@ -42,14 +44,14 @@ public class Log {
     @Step("{0}")
     public static void info(String message) {
         screenshotsRule.setName("Console");
-        LOGGER.addAppender(screenshotsRule);
+        LOGGER.addAppender(ansiConsoleAppender);
         LOGGER.setLevel(Level.INFO);
         LOGGER.info(message);
     }
 
     public static void debug(String message) {
         screenshotsRule.setName("Console");
-        LOGGER.addAppender(screenshotsRule);
+        LOGGER.addAppender(ansiConsoleAppender);
         LOGGER.setLevel(Level.DEBUG);
         LOGGER.debug(message);
     }
