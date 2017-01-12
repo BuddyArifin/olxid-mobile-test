e06580bf851f7b7e9937b9745a376e1c6908b9dd
Buddy Arifin <buddy.arifin@olx.co.id>
2017-01-12 14:06:17 +0700

    delete skip test on retry mode

--- src/main/java/listeners/Retry.java
+++ src/main/java/listeners/Retry.java
@@ -1,12 +1,17 @@
 package listeners;
 
 import org.testng.IRetryAnalyzer;
+import org.testng.ITestNGMethod;
 import org.testng.ITestResult;
 import ru.yandex.qatools.allure.Allure;
 import ru.yandex.qatools.allure.events.ClearStepStorageEvent;
 import ru.yandex.qatools.allure.events.RemoveAttachmentsEvent;
+import ru.yandex.qatools.allure.events.StepCanceledEvent;
+import ru.yandex.qatools.allure.events.TestCaseCanceledEvent;
 import ru.yandex.qatools.allure.utils.AnnotationManager;
 import utils.Log;
+
+import java.util.Iterator;
 
 /**
  * Created by buddyarifin on 7/22/16.
@@ -23,10 +28,24 @@ public class Retry implements IRetryAnalyzer {
                     .getMethod().getAnnotations());
             Allure.LIFECYCLE.fire(new RemoveAttachmentsEvent(".*"));
             Allure.LIFECYCLE.fire(new ClearStepStorageEvent());
-            Log.debug("######  Retry failed tc-scenarios : " + testAnnotationAllure.getTitle() + " ######");
+            deleteSkippedTestOnRetry(result);
+            Log.info("######  Retry failed tc-scenarios : " + testAnnotationAllure.getTitle() + " ######");
             retryCount++;
             return true;
         }
         return false;
+    }
+
+    private void deleteSkippedTestOnRetry(ITestResult tr) {
+        Iterator<ITestResult> listOfSkippedTest = tr.getTestContext().getSkippedTests().getAllResults().iterator();
+        while (listOfSkippedTest.hasNext()) {
+            ITestResult m_skipped_test = listOfSkippedTest.next();
+            ITestNGMethod m_skipped = m_skipped_test.getMethod();
+            if (tr.getTestContext().getSkippedTests().getResults(m_skipped).size() > 1){
+                listOfSkippedTest.remove();
+            } else if (tr.getTestContext().getSkippedTests().getResults(m_skipped).size() > 0) {
+                listOfSkippedTest.remove();
+            }
+        }
     }
 }
